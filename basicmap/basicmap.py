e3404b1f36f48857b5344df82528d0de0b768d61
Ajay Baranwal <aj@tugun.us>
2016-03-21 21:16:50 -0700

    reorganized

---
+++ basicmap/basicmap.py
@@ -0,0 +1,20 @@
+#from google.appengine.api import users
+
+import webapp2
+#import os
+from google.appengine.ext.webapp import template
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		#self.response.write('Hello world!')
+		template_values = {
+		}
+
+		path = 'index.html' 
+		self.response.out.write(template.render(path, template_values))
+		#path = 'htmlfiles/map.html' 
+		#self.response.out.write(template.render(path, template_values))
+
+app = webapp2.WSGIApplication([
+	('/', MainPage),
+], debug=True)
